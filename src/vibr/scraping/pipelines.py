e0e8c5f4bd7e40550adb6167a89179220baf6913
Christian DiMare <frndlytm@gmail.com>
2022-03-06 14:35:10 -0500

    Stage the missing save.

--- src/vibr/scraping/pipelines.py
+++ src/vibr/scraping/pipelines.py
@@ -5,9 +5,43 @@
 
 
 # useful for handling different item types with a single interface
+
+import json
+
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
-class ScrapingPipeline:
+class DuplicatesPipeline:
+    def __init__(self):
+        self.ids_seen = set()
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter['id'] in self.ids_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.ids_seen.add(adapter['id'])
+            return item
+
+
+class MSDFilterPipeline:
+    """
+    MSDFilterPipeline is a step to compare with the Million Songs Dataset
+    like the paper while extracting."
+    """
+    def process_item(self, item, spider):
+        ...
+
+
+class JsonLinesWriterPipeline:
+    def open_spider(self, spider):
+        self.file = open('items.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_album(self, item, spider):
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        self.file.write(line)
         return item
