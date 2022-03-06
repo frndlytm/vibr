594a279557c47171ed28682bbc2696202f222806
Christian DiMare <frndlytm@gmail.com>
2022-03-06 14:38:55 -0500

    Stage comments for the spider flow.

--- src/vibr/scraping/spiders/allmusic.py
+++ src/vibr/scraping/spiders/allmusic.py
@@ -7,8 +7,20 @@ class AllmusicSpider(SitemapSpider):
     sitemap_urls = ["https://www.allmusic.com/sitemap.xml"]
     allowed_domains = ['allmusic.com']
     sitemap_rules = [
-        ("/song/", "parse_song",),
+        ("/album/", "parse_album",),
     ]
 
+    def parse_album(self, response):
+        # Extract album info and yield it to the pipeline stage
+
+
+        # Get all the songs on the page
+
+        # yield a scrapy.Request(song/:id/attributes, self.parse_song)
+        ...
+
     def parse_song(self, response):
-        pass
+        # Extract attributes
+
+        # yield to pipeline
+        ...
