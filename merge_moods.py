6c48ef2d87fc3005f8a229bdc5c09726483ff898
adrian-bisberg <bisberg.a@northeastern.edu>
2022-04-03 15:04:57 -0400

    fixed merging based on album moods

--- merge_moods.py
+++ merge_moods.py
@@ -17,14 +17,10 @@ def merge_moods():
             album_moods = []
             for key, val in obj.items():
                 if(key == "moods"):
-                    album_moods = val
+                    album_moods = clean_moods(val)
                 if(key == "title"):
                     album_name = val.strip()
             album_and_moods[album_name] = album_moods
-
-    #Test - moods data is currentlyempty so fill out one to try out
-    album_and_moods['The Greatest Songs Ever Written'] = ['Animated','Dramatic', 'Effervescent']
-    #indexes: 8, 41, 49
 
     #create a dictionary mapping of each mood to an index in order of moods.txt
     moods_dict = {}
@@ -44,11 +40,8 @@ def merge_moods():
             new_entry.append(row[2]) #add song id
 
             #get album moods
-            moods_raw = get_moods(album_and_moods,row[5])
-            if moods_raw is None:
-                moods_encode = []
-            else:
-                moods_encode = moods_to_array(moods_dict,moods_raw)
+            album_name = row[5].strip()
+            moods_encode = moods_to_array(moods_dict, get_moods(album_and_moods, album_name))
             new_entry.append(moods_encode)
 
             song_id_to_moods.append(new_entry)
@@ -56,33 +49,42 @@ def merge_moods():
 
     #save an numpy array
     song_id_to_moods = np.array(song_id_to_moods, dtype=object)
-    print(song_id_to_moods[0])
+    print(song_id_to_moods[1])
     np.save('data/moods_target.npy', song_id_to_moods, allow_pickle=True, fix_imports=True)
 
 
+def clean_moods(moods_list):
+    out = []
+    for mood in moods_list:
+        if mood.split()[0] == "Would":
+            return None
+        out.append(mood.split()[0])
+    return out
+
+
 def get_moods(album_and_moods, album_name):
-    """
-    get the moods for an album
-    """
-    try:
+    if album_name not in album_and_moods.keys():
+        return None
+    else:
         return album_and_moods[album_name]
-    except:
-       return None
+
 
 def moods_to_array(moods_dict, example_moods):
     """
     convert list of moods from moods.txt into a one hot
     encoded array for every possible mood
     """
+    if not example_moods:
+        return None
+
     out = [0 for i in range(len(moods_dict.keys()))]
     for ex in example_moods:
         index = None
         try:
             index = moods_dict[ex]
+            out[index] = 1
         except:
             Exception("invlaid mood value: ", ex)
-            return None
-        out[index] = 1
 
     return out
 
